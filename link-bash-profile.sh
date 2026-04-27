da93e91fdbfa2c524f930d8a04d8b955141f77c8
Martin Schumann <martin.schumann@contao-garage.de>
2026-04-27 22:17:52 +0200

    Replace flag for relative symlink by parameter expansion

--- link-bash-profile.sh
+++ link-bash-profile.sh
@@ -8,4 +8,6 @@ source "$directory/lib.message.sh"
 [[ -h "${HOME}/.bash_profile" ]] && _printMessage "Start up file \".bash_profile\" already symlinked." "info" && exit 1
 [[ -f "${HOME}/.bash_profile" ]] && _printMessage "Start up file \".bash_profile\" already exists. Cannot symlink." "error" && exit 1
 
-cd "${HOME}" && /bin/ln -s -r "${directory}/.bash_profile" && _printMessage "Symlinked start up file \".bash_profile\" to repo's start up file." "success" && exit 0
+cd "${HOME}"
+/bin/ln -s "${directory/#${HOME}\//}/.bash_profile" && _printMessage "Symlinked start up file \".bash_profile\" to repo's start up file." "success"
+exit 0
